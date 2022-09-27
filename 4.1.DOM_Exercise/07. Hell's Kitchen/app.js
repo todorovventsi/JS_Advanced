3a36e7cd92118da317a944d54658c58d313bcb2b
Ventsislav Todorov <todorov.ventsi@gmail.com>
2022-09-27 14:10:09 +0300

    DOM Manipulation and events LAB

--- 4.1.DOM_Exercise/07. Hell's Kitchen/app.js
+++ 4.1.DOM_Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,9 @@
-function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+let restaurantsList = {};
 
-   function onClick () {
-      //   TODO:
-      
-   }
+function solve() {
+  document.querySelector("#btnSend").addEventListener("click", onClick);
+
+  function onClick() {
+    //   TODO:
+  }
 }
