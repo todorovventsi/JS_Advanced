d2ef40bdc88470e69694c26e4c9a0e6b9aff4ebb
Ventsislav Todorov <todorov.ventsi@gmail.com>
2022-10-01 17:49:07 +0300

    ...

--- 5.1.DOM_Manipulations_and_Events_Exercise/05. Encode-and-Decode-Messages/app.js
+++ 5.1.DOM_Manipulations_and_Events_Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,36 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  let [inputTextAreaRef, displayTextAreaRef] = Array.from(
+    document.querySelectorAll("#main textarea")
+  );
+  let [encodeBtnRef, decodeBtnRef] = Array.from(
+    document.querySelectorAll("#main button")
+  );
+
+  encodeBtnRef.addEventListener("click", encodeAndSend);
+  decodeBtnRef.addEventListener("click", decodeAndShow);
+
+  function encodeAndSend(event) {
+    let message = inputTextAreaRef.value;
+    let encodedM = "";
+    for (let char of message) {
+      let charAscii = char.charCodeAt();
+      let newChar = String.fromCharCode(charAscii + 1);
+      encodedM += newChar;
+    }
+    displayTextAreaRef.value = encodedM;
+    inputTextAreaRef.value = "";
+    debugger;
+  }
+
+  function decodeAndShow(event) {
+    let toDecode = displayTextAreaRef.value;
+    let decodedM = "";
+    for (let char of toDecode) {
+      let charAscii = char.charCodeAt();
+      let newChar = String.fromCharCode(charAscii - 1);
+      decodedM += newChar;
+    }
+    displayTextAreaRef.value = decodedM;
+    let test = "1";
+  }
 }
